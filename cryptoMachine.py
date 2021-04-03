d78e9a346f31e841dba8f7a92ffb6fb0541e3b49
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2021-04-03 19:07:12 +0530

    Add files via upload

---
+++ cryptoMachine.py
@@ -0,0 +1,23 @@
+def machine():
+    keys = 'abcdefghijklmnopqrstuvwxyz !'
+    value = keys[-1] + keys[0:-1]
+
+    encryptDict = dict(zip(keys,value))
+    decryptDict = dict(zip(value,keys))
+
+    message = input("please enter your secret message:")
+    mode = input("please enter the mode : Encode(E) OR Decode(D)")
+
+    if mode.upper() == 'E':
+        newMessage = ''.join([encryptDict[letter]
+        for letter in message.lower()])
+
+    elif mode.upper() == 'D':
+        newMessage = ''.join([decryptDict[letter]
+        for letter in message.lower()])
+
+    else:
+        print("Please enter a correct choice")
+
+    return newMessage
+print(machine())                
